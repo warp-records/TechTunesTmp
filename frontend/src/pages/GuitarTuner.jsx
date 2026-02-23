7519a840699fdaefc6a2f184406ba78b9543f9d5
rift <32559038+warp-records@users.noreply.github.com>
2026-02-23 18:21:46 -0500

    make into props

--- frontend/src/pages/GuitarTuner.jsx
+++ frontend/src/pages/GuitarTuner.jsx
@@ -1,35 +1,46 @@
+import { useState } from 'react'
+
 import './GuitarTuner.css'
 
-import guitarHead from '../assets/Tuner/Buttons/Base Guitar Head.png'
-import eLeftString from '../assets/Tuner/Strings/E Left String Clicked.png'
-import aString from '../assets/Tuner/Strings/A String Clicked.png'
-import dString from '../assets/Tuner/Strings/D String Clicked.png'
-import gString from '../assets/Tuner/Strings/G String Clicked.png'
-import bString from '../assets/Tuner/Strings/B String Clicked.png'
-import eRightString from '../assets/Tuner/Strings/E Right String Clicked.png'
-
 export default function GuitarTuner() {
+  let [activeNote, setActiveNote] = useState("string-a");
+  const stringLetters = [
+    "e-low",
+    "a",
+    "d",
+    "g",
+    "b",
+    "e-high",
+  ]
+  
   return (
     <div className="guitar-container">
       <div className="guitar-headstock">
-        <img src={guitarHead} alt="Guitar headstock" className="headstock-image" />
+        <div className="headstock-image"></div>
         <div className="guitar-strings">
-          <img src={eLeftString} alt="E low string" className="guitar-string string-e-low" id="string-e-low" />
-          <img src={aString} alt="A string" className="guitar-string string-a" id="string-a" />
-          <img src={dString} alt="D string" className="guitar-string string-d" id="string-d" />
-          <img src={gString} alt="G string" className="guitar-string string-g active" id="string-g" />
-          <img src={bString} alt="B string" className="guitar-string string-b" id="string-b" />
-          <img src={eRightString} alt="E high string" className="guitar-string string-e-high" id="string-e-high" />
+          {stringLetters.map((letter, idx) => ( 
+            <GuitarString note={letter} activeNote={activeNote} key={idx} />
+           ))}
         </div>
         <div className="string-letters">
-          <div className="string-letter letter-e-low" id="letter-e-low">E</div>
-          <div className="string-letter letter-a" id="letter-a">A</div>
-          <div className="string-letter letter-d" id="letter-d">D</div>
-          <div className="string-letter letter-g active" id="letter-g">G</div>
-          <div className="string-letter letter-b" id="letter-b">B</div>
-          <div className="string-letter letter-e-high" id="letter-e-high">E</div>
+          {stringLetters.map((letter, idx) => ( 
+            <StringLetter note={letter} activeNote={activeNote} key={idx} />
+           ))}
         </div>
       </div>
     </div>
   )
 }
+
+export function GuitarString({ note, activeNote }) {
+  return (
+    <div className={`guitar-string string-${note} ${note == activeNote ? 'active' : ''}`} id={note}></div>
+  )
+}
+
+export function StringLetter({ note, activeNote }) {
+  return (
+    <div className={`string-letter letter-${note} ${note == activeNote ? 'active' : ''}`} id={note}>{note[0].toUpperCase()}</div>
+  )
+}
+
