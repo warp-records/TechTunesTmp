23d72c9889347c3898068419ac91f8db937bf423
rift <32559038+warp-records@users.noreply.github.com>
2026-03-04 18:15:18 -0500

    create structure of note

--- frontend/src/pages/Lesson/Lesson.jsx
+++ frontend/src/pages/Lesson/Lesson.jsx
@@ -5,6 +5,11 @@ import NeonFrame from '../../assets/Lesson Page Assets/Neon Board Frame.png'
 import Board from '../../assets/Lesson Page Assets/Board.png'
 import StringNames from '../../assets/Lesson Page Assets/String Names.png'
 
+const noteImages = import.meta.glob(
+  '../../assets/Lesson Page Assets/Notes/**/*.png',
+  { eager: true, import: 'default' }
+)
+
 export default function Lesson() {
   
   return (
@@ -18,16 +23,24 @@ export default function Lesson() {
       <img className="layer-string-names" src={StringNames} alt="String Names" />
     </div>
     
+      <Note string="E_HIGH" fret="3" glow={false} />
     </>
   )
 }
 
 /*
   progress: decimal from 0 - 1 
-  string: string path note takes ['e_low', 'a', 'd', 'g', 'b', 'e_high']
+  string: string path note takes ['E', 'A', 'D', 'G', 'B', 'E_HIGH']
   fret: fret number from 1 - 5 (for now)
   glowing: use glowing or non glowing image
 */
-export function Note({ progress, string, fret, glowing }) {
+export function Note({ progress, string, fret, glow }) {
+  if (string == 'E_HIGH') {
+    string = "E2"
+  }
   
+  const noteImgPath = `../../assets/Lesson Page Assets/Notes/${string}/${fret}${glow ? " Glow" : ""}.png`;
+  return (
+    <img src={noteImages[noteImgPath]} className={`note ${glow ? "glow" : ""}`}></img>
+  )
 }
