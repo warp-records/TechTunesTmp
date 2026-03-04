107b3487646dfe74b7e346729cd10b49871393f2
rift <32559038+warp-records@users.noreply.github.com>
2026-03-04 18:52:46 -0500

    complete note prop

--- frontend/src/pages/Lesson/Lesson.jsx
+++ frontend/src/pages/Lesson/Lesson.jsx
@@ -23,7 +23,7 @@ export default function Lesson() {
         <img className="layer-string-names" src={StringNames} alt="String Names" />
       </div>
       
-      <Note string="G" fret="3" glow={false} />
+      <Note string="B" fret="3" glow={false} progress={0.5} />
     </>
   )
 }
@@ -40,44 +40,33 @@ export function Note({ progress, string, fret, glow }) {
     string_filename = "E2";
   }
   
-  let stringIdx = null;
-  switch (string) {
-    case 'E':
-      stringIdx = 0
-      break
-    
-    case 'A':
-      stringIdx = 1
-      break
-    
-    case 'D':
-      stringIdx = 2
-      break
-    
-    case 'G':
-      stringIdx = 3
-      break
-      
-    case 'B':
-      stringIdx = 4
-      break
-      
-    case 'E_HIGH':
-      stringIdx = 5
-      
+  let stringIdxMap = {
+    'E': 0,
+    'A': 1,
+    'D': 2,
+    'G': 3,
+    'B': 4,
+    'E_HIGH': 5
   }
+  let stringIdx = stringIdxMap[string];
+  
+  const xStart = 63.5 + 11.5 * stringIdx;
+  const yStart = 0;
   
   // in viewports
   const xEnd = 52 + stringIdx * 16.05;
   const yEnd = 91;
+  
+  const currX = xStart + (xEnd - xStart) * progress
+  const currY = yStart + (yEnd - yStart) * progress
   
   const noteImgPath = `../../assets/Lesson Page Assets/Notes/${string_filename}/${fret}${glow ? " Glow" : ""}.png`;
   return (
     <img src={noteImages[noteImgPath]} className={`note ${glow ? "glow" : ""}`}
       style={{
         position: 'absolute',
-        left: `${xEnd}vh`,
-        top: `91vh`
+        left: `${currX}vh`,
+        top: `${currY}vh`
     }}
     ></img>
   )
