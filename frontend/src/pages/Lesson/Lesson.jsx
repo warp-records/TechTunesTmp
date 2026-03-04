24c1f321cee4ddd098948969e093923892ad5946
rift <32559038+warp-records@users.noreply.github.com>
2026-03-04 16:40:31 -0500

    string names

--- frontend/src/pages/Lesson/Lesson.jsx
+++ frontend/src/pages/Lesson/Lesson.jsx
@@ -3,6 +3,7 @@ import HomeButton from '../../components/HomeButton'
 import Strings from '../../assets/Lesson Page Assets/Strings.png'
 import NeonFrame from '../../assets/Lesson Page Assets/Neon Board Frame.png'
 import Board from '../../assets/Lesson Page Assets/Board.png'
+import StringNames from '../../assets/Lesson Page Assets/String Names.png'
 
 export default function Lesson() {
   return (
@@ -13,7 +14,9 @@ export default function Lesson() {
       <img className="layer-board" src={Board} alt="Board" />
       <img className="layer-frame" src={NeonFrame} alt="Neon Board Frame" />
       <img className="layer-strings" src={Strings} alt="Strings" />
+      <img className="layer-string-names" src={StringNames} alt="String Names" />
     </div>
+    
     </>
   )
 }
